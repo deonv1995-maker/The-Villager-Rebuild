550190085a70efbe7fc258d968b76ce2867e742b
deonv1995-maker <deonv1995@gmail.com>
2026-08-28 21:02:40 +0200

    Add canonical production asset paths

---
+++ src/data/AssetPaths.js
@@ -0,0 +1,16 @@
+export const ASSET_PATHS = Object.freeze({
+  ranger: Object.freeze({
+    model: './assets/kaykit/adventurers/Ranger.glb',
+    movementBasic: './assets/kaykit/animations/Rig_Medium_MovementBasic.glb',
+    general: './assets/kaykit/animations/Rig_Medium_General.glb'
+  }),
+  forest: Object.freeze({
+    treeBroad: './assets/kaykit/forest/Tree_1_A_Color1.gltf',
+    treeTall: './assets/kaykit/forest/Tree_2_A_Color1.gltf',
+    rock: './assets/kaykit/forest/Rock_1_A_Color1.gltf'
+  }),
+  cliffs: Object.freeze({
+    large: './assets/kenney/nature/cliff_large_rock.glb',
+    rock: './assets/kenney/nature/rock_largeA.glb'
+  })
+});
