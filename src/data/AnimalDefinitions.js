4f08056aeb91b902da2c2d8d6061483ba22022e8
deonv1995-maker <deonv1995@gmail.com>
2026-08-29 07:22:40 +0200

    Add boar gameplay definition

---
+++ src/data/AnimalDefinitions.js
@@ -0,0 +1,11 @@
+export const ANIMAL_DEFINITIONS = Object.freeze({
+  boar: Object.freeze({
+    id: 'boar',
+    label: 'Boar',
+    maxHealth: 2,
+    spearDamage: 1,
+    attackRange: 2.8,
+    wanderRadius: 2.4,
+    wanderSpeed: 0.55
+  })
+});
