bcfb0197292c55dca7f74d75ebee781e5645aa9b
deonv1995-maker <deonv1995@gmail.com>
2026-08-29 17:26:07 +0200

    Add offline install shell

---
+++ public/sw.js
@@ -0,0 +1,63 @@
+const CACHE_PREFIX = 'villager-rebuild-';
+const CACHE_NAME = `${CACHE_PREFIX}0.3.1`;
+const SHELL = [
+  './',
+  './manifest.webmanifest',
+  './icons/villager-192.png',
+  './icons/villager-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put('./', copy));
+          return response;
+        })
+        .catch(() => caches.match('./'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then(cached => {
+      const network = fetch(request)
+        .then(response => {
+          if (response.ok && response.type === 'basic') {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+          }
+          return response;
+        })
+        .catch(() => cached);
+      return cached ?? network;
+    })
+  );
+});
