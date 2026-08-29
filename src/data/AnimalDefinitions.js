dc4cd843a9fe39890a10585e9341e7838cf6eb8a
deonv1995-maker <deonv1995@gmail.com>
2026-08-29 17:27:19 +0200

    Select Wild Pig for Day 1 hunt

--- src/data/AnimalDefinitions.js
+++ src/data/AnimalDefinitions.js
@@ -1,13 +1,19 @@
 export const ANIMAL_DEFINITIONS = Object.freeze({
-  boar: Object.freeze({
-    id: 'boar',
-    label: 'Boar',
+  dayOneHunt: Object.freeze({
+    id: 'wild_pig',
+    label: 'Wild Pig',
     maxHealth: 2,
     spearDamage: 1,
     attackRange: 2.8,
-    wanderRadius: 2.4,
-    wanderSpeed: 0.55,
+    wanderRadius: 2.7,
+    wanderSpeed: 0.52,
     harvestRange: 2.5,
+    presentation: Object.freeze({
+      assetKey: 'qiwiiPig',
+      targetLength: 1.75,
+      maxHeight: 1.12,
+      yawOffset: 0
+    }),
     loot: Object.freeze({
       itemId: 'meat',
       label: 'Raw Meat',
