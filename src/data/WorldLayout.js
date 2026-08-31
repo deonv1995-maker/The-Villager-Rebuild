b181c49d1ee1fdb0b61e31e81aa527ee4dac5af8
deonv1995-maker <deonv1995@gmail.com>
2026-08-31 08:20:43 +0200

    Seed grass and extra basic resources

--- src/data/WorldLayout.js
+++ src/data/WorldLayout.js
@@ -4,14 +4,24 @@ export const WORLD_LAYOUT = Object.freeze({
   dayOneResources: Object.freeze([
     Object.freeze(['stick', 1.6, 78.5]),
     Object.freeze(['stone', -1.8, 78.1]),
+    Object.freeze(['grass', -3.1, 76.2]),
     Object.freeze(['stick', 4.6, 73.8]),
     Object.freeze(['stone', -5.2, 73.2]),
+    Object.freeze(['grass', 5.7, 71.4]),
     Object.freeze(['stick', -3.5, 69.3]),
+    Object.freeze(['stone', 6.2, 68.1]),
+    Object.freeze(['grass', -6.4, 66.9]),
     Object.freeze(['stick', 6.9, 66.1]),
     Object.freeze(['stone', 3.2, 63.4]),
+    Object.freeze(['grass', 1.2, 62.1]),
     Object.freeze(['stick', -7.4, 61.4]),
     Object.freeze(['stone', 8.4, 58.8]),
-    Object.freeze(['stick', -1.6, 56.9])
+    Object.freeze(['grass', -8.1, 57.8]),
+    Object.freeze(['stick', -1.6, 56.9]),
+    Object.freeze(['stone', 5.8, 52.7]),
+    Object.freeze(['stick', -5.9, 51.4]),
+    Object.freeze(['grass', 1.5, 49.8]),
+    Object.freeze(['stone', -2.8, 47.1])
   ])
 });
 
