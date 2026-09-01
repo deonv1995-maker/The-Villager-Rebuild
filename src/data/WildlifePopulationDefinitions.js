ba496f7c4bc1fbafbf8e326c942e91dbe078dcc8
deonv1995-maker <deonv1995@gmail.com>
2026-09-01 14:05:58 +0200

    Add renewable wildlife respawn policy

--- src/data/WildlifePopulationDefinitions.js
+++ src/data/WildlifePopulationDefinitions.js
@@ -1,6 +1,8 @@
 export const WILDLIFE_POPULATION = Object.freeze({
   seed: 0x91c47b,
   spawnExclusionRadius: 26,
+  respawnClearanceRadius: 48,
+  respawnRetryDelay: 12,
   species: Object.freeze({
     wildPig: Object.freeze({
       count: 8,
@@ -8,7 +10,8 @@ export const WILDLIFE_POPULATION = Object.freeze({
       groupRadius: 5.5,
       minGroupSpacing: 24,
       maxSlope: 0.42,
-      habitat: 'shoreline'
+      habitat: 'shoreline',
+      respawnDelay: Object.freeze([90, 150])
     }),
     deer: Object.freeze({
       count: 10,
@@ -16,7 +19,8 @@ export const WILDLIFE_POPULATION = Object.freeze({
       groupRadius: 7.5,
       minGroupSpacing: 34,
       maxSlope: 0.34,
-      habitat: 'open-field'
+      habitat: 'open-field',
+      respawnDelay: Object.freeze([120, 210])
     }),
     rabbit: Object.freeze({
       count: 16,
@@ -24,7 +28,8 @@ export const WILDLIFE_POPULATION = Object.freeze({
       groupRadius: 6,
       minGroupSpacing: 22,
       maxSlope: 0.38,
-      habitat: 'forest'
+      habitat: 'forest',
+      respawnDelay: Object.freeze([55, 100])
     }),
     fox: Object.freeze({
       count: 2,
@@ -32,7 +37,8 @@ export const WILDLIFE_POPULATION = Object.freeze({
       groupRadius: 0,
       minGroupSpacing: 52,
       maxSlope: 0.46,
-      habitat: 'forest'
+      habitat: 'forest',
+      respawnDelay: Object.freeze([180, 300])
     }),
     wolf: Object.freeze({
       count: 1,
@@ -40,7 +46,8 @@ export const WILDLIFE_POPULATION = Object.freeze({
       groupRadius: 0,
       minGroupSpacing: 70,
       maxSlope: 0.48,
-      habitat: 'deep-forest'
+      habitat: 'deep-forest',
+      respawnDelay: Object.freeze([240, 360])
     })
   })
 });
