e428087d67b99393cc088db44cd6af60587a8fd6
deonv1995-maker <deonv1995@gmail.com>
2026-09-01 13:54:18 +0200

    Place wildlife by habitat and social groups

--- src/data/WildlifePopulationDefinitions.js
+++ src/data/WildlifePopulationDefinitions.js
@@ -3,22 +3,44 @@ export const WILDLIFE_POPULATION = Object.freeze({
   spawnExclusionRadius: 26,
   species: Object.freeze({
     wildPig: Object.freeze({
-      count: 6,
-      minSpacing: 22,
-      maxSlope: 0.48,
-      habitat: 'mixed'
+      count: 8,
+      groupSize: Object.freeze([2, 3]),
+      groupRadius: 5.5,
+      minGroupSpacing: 24,
+      maxSlope: 0.42,
+      habitat: 'shoreline'
     }),
     deer: Object.freeze({
-      count: 7,
-      minSpacing: 27,
-      maxSlope: 0.44,
-      habitat: 'woodland-edge'
+      count: 10,
+      groupSize: Object.freeze([3, 5]),
+      groupRadius: 7.5,
+      minGroupSpacing: 34,
+      maxSlope: 0.34,
+      habitat: 'open-field'
     }),
     rabbit: Object.freeze({
-      count: 12,
-      minSpacing: 15,
-      maxSlope: 0.36,
-      habitat: 'grassland'
+      count: 16,
+      groupSize: Object.freeze([2, 4]),
+      groupRadius: 6,
+      minGroupSpacing: 22,
+      maxSlope: 0.38,
+      habitat: 'forest'
+    }),
+    fox: Object.freeze({
+      count: 2,
+      groupSize: Object.freeze([1, 1]),
+      groupRadius: 0,
+      minGroupSpacing: 52,
+      maxSlope: 0.46,
+      habitat: 'forest'
+    }),
+    wolf: Object.freeze({
+      count: 1,
+      groupSize: Object.freeze([1, 1]),
+      groupRadius: 0,
+      minGroupSpacing: 70,
+      maxSlope: 0.48,
+      habitat: 'deep-forest'
     })
   })
 });
