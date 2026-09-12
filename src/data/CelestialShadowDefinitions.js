658ca5e8557452373aa3a0110cc36c31966267e2
deonv1995-maker <deonv1995@gmail.com>
2026-09-12 13:18:57 +0200

    Add low-cost celestial shadow budget

---
+++ src/data/CelestialShadowDefinitions.js
@@ -0,0 +1,10 @@
+export const CELESTIAL_SHADOWS = Object.freeze({
+  mapSize: 512,
+  cameraHalfSize: 28,
+  cameraNear: 0.5,
+  cameraFar: 96,
+  refreshHz: 10,
+  casterRescanSeconds: 1,
+  bias: -0.00065,
+  normalBias: 0.045
+});
