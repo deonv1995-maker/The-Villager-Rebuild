1ebf1ca642e3c0a17539611d7dbeab93494d2ff0
deonv1995-maker <deonv1995@gmail.com>
2026-09-12 18:30:34 +0200

    Add Sprout companion tuning data

---
+++ src/data/SproutCompanionDefinitions.js
@@ -0,0 +1,19 @@
+export const SPROUT_COMPANION = Object.freeze({
+  followDistance: 1.65,
+  followSideOffset: 0.78,
+  hoverHeight: 1.12,
+  followSpeed: 4.2,
+  collectionMoveSpeed: 4.8,
+  catchUpSpeed: 7.2,
+  catchUpDistance: 4.8,
+  hardCatchUpDistance: 11.5,
+  collisionRadius: 0.28,
+  collisionHeight: 0.82,
+  collectionRadius: 6.25,
+  beamRange: 2.15,
+  scanIntervalSeconds: 0.18,
+  collectionCooldownSeconds: 0.12,
+  compressionSeconds: 0.52,
+  logCompressionSeconds: 0.78,
+  collectibleResourceIds: Object.freeze(['stick', 'stone', 'grass', 'log'])
+});
