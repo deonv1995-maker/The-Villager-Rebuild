d64d44c040899e6da4bc5708542a8a7d3f6df019
deonv1995-maker <deonv1995@gmail.com>
2026-09-16 20:59:42 +0200

    Tag resources for storage routing

--- src/data/ResourceDefinitions.js
+++ src/data/ResourceDefinitions.js
@@ -3,30 +3,35 @@ export const RESOURCE_DEFINITIONS = Object.freeze({
     id: 'stick',
     label: 'Stick',
     pickupQuantity: 1,
-    storage: 'inventory'
+    storage: 'inventory',
+    storageCategory: 'material'
   }),
   stone: Object.freeze({
     id: 'stone',
     label: 'Stone',
     pickupQuantity: 1,
-    storage: 'inventory'
+    storage: 'inventory',
+    storageCategory: 'material'
   }),
   grass: Object.freeze({
     id: 'grass',
     label: 'Grass',
     pickupQuantity: 1,
-    storage: 'inventory'
+    storage: 'inventory',
+    storageCategory: 'material'
   }),
   meat: Object.freeze({
     id: 'meat',
     label: 'Raw Meat',
     pickupQuantity: 1,
-    storage: 'inventory'
+    storage: 'inventory',
+    storageCategory: 'food'
   }),
   log: Object.freeze({
     id: 'log',
     label: 'Log',
     pickupQuantity: 1,
-    storage: 'inventory'
+    storage: 'inventory',
+    storageCategory: 'material'
   })
 });
