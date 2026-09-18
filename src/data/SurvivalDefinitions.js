27a18a3e3e91c6ea9b43a9bdfa50370e0dd0f83e
deonv1995-maker <deonv1995@gmail.com>
2026-09-18 07:00:38 +0200

    Add player survival tuning

---
+++ src/data/SurvivalDefinitions.js
@@ -0,0 +1,10 @@
+export const PLAYER_SURVIVAL = Object.freeze({
+  maxHealth: 100,
+  maxHunger: 100,
+  startingHealth: 100,
+  startingHunger: 100,
+  hungerLossPerWorldDay: 60,
+  starvationDamagePerWorldHour: 5,
+  recoveryHealth: 50,
+  recoveryHunger: 35
+});
