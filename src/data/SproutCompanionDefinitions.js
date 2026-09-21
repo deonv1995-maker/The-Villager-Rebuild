0f676fc3b64b3b3de6e50540354154299147f39b
deonv1995-maker <deonv1995@gmail.com>
2026-09-21 16:04:34 +0200

    Tune fast Sprout shoe transformation

--- src/data/SproutCompanionDefinitions.js
+++ src/data/SproutCompanionDefinitions.js
@@ -32,6 +32,7 @@ export const SPROUT_COMPANION = Object.freeze({
   energyRechargePerSecond: 0.35,
   flightEnergyPerSecond: 8,
   flightMinimumEnergy: 1,
+  flightTransformSeconds: 0.16,
   resourceScanRange: 34,
   resourceScanHoldSeconds: 3.2,
   undergroundScanRange: 56,
