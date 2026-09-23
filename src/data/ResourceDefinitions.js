20e6da4e61baa0a3941a5497c757153bb709ec10
deonv1995-maker <deonv1995@gmail.com>
2026-09-23 09:13:17 +0200

    Define slot metadata for inventory resources

--- src/data/ResourceDefinitions.js
+++ src/data/ResourceDefinitions.js
@@ -4,42 +4,54 @@ export const RESOURCE_DEFINITIONS = Object.freeze({
     label: 'Stick',
     pickupQuantity: 1,
     storage: 'inventory',
-    storageCategory: 'material'
+    storageCategory: 'material',
+    stackSize: 14,
+    slotCost: 1
   }),
   stone: Object.freeze({
     id: 'stone',
     label: 'Stone',
     pickupQuantity: 1,
     storage: 'inventory',
-    storageCategory: 'material'
+    storageCategory: 'material',
+    stackSize: 14,
+    slotCost: 1
   }),
   copper: Object.freeze({
     id: 'copper',
     label: 'Copper Ore',
     pickupQuantity: 1,
     storage: 'inventory',
-    storageCategory: 'material'
+    storageCategory: 'material',
+    stackSize: 14,
+    slotCost: 1
   }),
   iron: Object.freeze({
     id: 'iron',
     label: 'Iron Ore',
     pickupQuantity: 1,
     storage: 'inventory',
-    storageCategory: 'material'
+    storageCategory: 'material',
+    stackSize: 14,
+    slotCost: 1
   }),
   diamond: Object.freeze({
     id: 'diamond',
     label: 'Diamond Ore',
     pickupQuantity: 1,
     storage: 'inventory',
-    storageCategory: 'material'
+    storageCategory: 'material',
+    stackSize: 14,
+    slotCost: 1
   }),
   grass: Object.freeze({
     id: 'grass',
     label: 'Grass',
     pickupQuantity: 1,
     storage: 'inventory',
-    storageCategory: 'material'
+    storageCategory: 'material',
+    stackSize: 14,
+    slotCost: 1
   }),
   meat: Object.freeze({
     id: 'meat',
@@ -47,6 +59,8 @@ export const RESOURCE_DEFINITIONS = Object.freeze({
     pickupQuantity: 1,
     storage: 'inventory',
     storageCategory: 'food',
+    stackSize: 14,
+    slotCost: 1,
     food: Object.freeze({
       edible: false
     })
@@ -57,6 +71,8 @@ export const RESOURCE_DEFINITIONS = Object.freeze({
     pickupQuantity: 1,
     storage: 'inventory',
     storageCategory: 'food',
+    stackSize: 14,
+    slotCost: 1,
     food: Object.freeze({
       edible: true,
       hungerRestore: 45
@@ -68,6 +84,8 @@ export const RESOURCE_DEFINITIONS = Object.freeze({
     pickupQuantity: 1,
     storage: 'inventory',
     storageCategory: 'food',
+    stackSize: 14,
+    slotCost: 1,
     food: Object.freeze({
       edible: false
     })
@@ -78,6 +96,8 @@ export const RESOURCE_DEFINITIONS = Object.freeze({
     pickupQuantity: 1,
     storage: 'inventory',
     storageCategory: 'food',
+    stackSize: 14,
+    slotCost: 1,
     food: Object.freeze({
       edible: true,
       hungerRestore: 60
@@ -88,20 +108,26 @@ export const RESOURCE_DEFINITIONS = Object.freeze({
     label: 'Ancient Relic',
     pickupQuantity: 1,
     storage: 'inventory',
-    storageCategory: 'treasure'
+    storageCategory: 'relic',
+    stackSize: 1,
+    slotCost: 0
   }),
   sprout_shard: Object.freeze({
     id: 'sprout_shard',
     label: 'Sprout Upgrade Shard',
     pickupQuantity: 1,
     storage: 'inventory',
-    storageCategory: 'progression'
+    storageCategory: 'currency',
+    stackSize: 1,
+    slotCost: 0
   }),
   log: Object.freeze({
     id: 'log',
     label: 'Log',
     pickupQuantity: 1,
     storage: 'inventory',
-    storageCategory: 'material'
+    storageCategory: 'material',
+    stackSize: 1,
+    slotCost: 1
   })
 });
